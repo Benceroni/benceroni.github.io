fe4a498471eef9438ca684c4878af230c88a57f4
benceroni <bel21032@byui.edu>
2022-06-15 18:23:02 -0600

    initial commit for weather page

--- lesson2/weather.js
+++ lesson2/weather.js
@@ -2,4 +2,3 @@ let currentDate = new Date();
 let cDay = currentDate.getDate();
 let cMonth = currentDate.getMonth() + 1;
 let cYear = currentDate.getFullYear();
-document.getElementsByClassName("date").innerHTML = cDay;
