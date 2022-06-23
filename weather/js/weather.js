6dee6e73b4a75f6d83b1b13fa561530f945d9c98
Benceroni <90892977+Benceroni@users.noreply.github.com>
2022-06-22 18:03:57 -0600

    yeah version control or summin yeah

--- weather/js/weather.js
+++ weather/js/weather.js
@@ -9,6 +9,13 @@ const daylist = [
   "Thursday",
   "Friday",
   "Saturday",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday ",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 const monthNames = [
   "January",
@@ -25,6 +32,12 @@ const monthNames = [
   "December",
 ];
 
+const tomorrow = daylist[day + 1];
+const day2 = daylist[day + 2];
+const day3 = daylist[day + 3];
+const day4 = daylist[day + 4];
+const day5 = daylist[day + 5];
+
 document.getElementById("displayDate").innerHTML =
   daylist[day] +
   ", " +
@@ -39,3 +52,9 @@ function toggleMenu() {
     .getElementsByClassName("navigation")[0]
     .classList.toggle("responsive");
 }
+
+document.getElementById("day1").innerHTML = tomorrow;
+document.getElementById("day2").innerHTML = day2;
+document.getElementById("day3").innerHTML = day3;
+document.getElementById("day4").innerHTML = day4;
+document.getElementById("day5").innerHTML = day5;
