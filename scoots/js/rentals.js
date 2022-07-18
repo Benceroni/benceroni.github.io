0ca8e6d70bbced363cb791334401ea946d26fe55
Benceroni <90892977+Benceroni@users.noreply.github.com>
2022-07-18 17:02:02 -0600

    pushing data

--- scoots/js/rentals.js
+++ scoots/js/rentals.js
@@ -1,1 +1,12 @@
-const requestURL = "data/rentals.json";
+const requestURL = "httsp://benceroni.github.io/data/rentals.json";
+
+
+fetch(requestURL)
+.then(function(response){
+    return response.json();
+})
+.then(function(jsonObject)){
+    console.table(jsonObject);
+    const scooters = jsonObject["scooters"];
+    console.log[scooters]
+}
